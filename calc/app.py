5be8e4ac7378c2d8e684332dba75fecfc1b23a53
Youssef Doss <youssefdoss97@gmail.com>
2022-11-30 15:25:16 -0500

    calc done

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,36 @@
 # Put your app in here.
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.get('/add')
+def add():
+    """Adds two parameter inputs"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return str(a + b)
+
+@app.get('/sub')
+def sub():
+    """Subtracts two parameter inputs"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return str(a - b)
+
+@app.get('/mult')
+def mult():
+    """Multiplies two parameter inputs"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return str(a * b)
+
+@app.get('/div')
+def div():
+    """Divides two parameter inputs"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    return str(a / b)
